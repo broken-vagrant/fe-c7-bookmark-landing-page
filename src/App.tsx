5ae79777585d8a751b94d0ed17bc723a3d44bbe0
zkindest <zkindest5@gmail.com>
2021-12-20 20:08:29 +0530

    add headerlinks,wip tabs component for features section

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import './App.css';
 import Button from './components/Button';
 import Header from './components/Header';
 
-
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
         <section className="relative flex flex-col lg:flex-row-reverse">
-          <div className="bg__hero-img relative basis-[60%]">
+          <div className="bg__hero-img relative basis-[60%] select-none pointer-events-none">
             <div className="relative lg:mr-20 mr-0 mt-10">
               <img src="./images/illustration-hero.svg" alt="hero" className="w-full h-full object-contain"></img>
             </div>
@@ -22,6 +21,38 @@ function App() {
                 <Button className="">Get it on Chrome</Button>
                 <Button themeColor="gray">Get it on Firefox</Button>
               </div>
+            </div>
+          </div>
+        </section>
+        <section className="relative mt-10 text-[1rem]">
+          <div className="w-[30%] mx-auto my-8 text-center">
+            <h2 className="font-medium text-[1.5em] my-4">
+              Features
+            </h2>
+            <p className="text-grayish-blue">
+              Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.
+            </p>
+          </div>
+          <div className="tabs ">
+            <div role="tablist" aria-label="Features" className="border-b-gray-300 border-solid border-b-[1px] max-w-[890px] mx-auto flex items-center justify-between text-lg text-grayish-blue">
+              <button role="tab" aria-selected="true" aria-controls="bookmarking-tab" id="bookmarking" className=" relative p-4 px-6  after:content-[''] after:absolute after:inset-0 after:border-b-transparent after:border-b-4 after:border-solid hover:text-soft-red active:text-very-dark-blue active:after:border-b-soft-red">
+                Simple Bookmarking
+              </button>
+              <button role="tab" aria-selected="true" aria-controls="speedy-searching-tab" id="speedy-searching" className="p-4 px-6 hover:text-very-dark-blue">
+                Speedy Searching
+              </button>
+              <button role="tab" aria-selected="true" aria-controls="easy-sharing-tab" id="easy-sharing" className="p-4 px-6 hover:text-very-dark-blue">
+                Easy Sharing
+              </button>
+            </div>
+            <div tabIndex={0} role="tabpanel" id="bookmarking-tab" aria-labelledby="bookmarking">
+
+            </div>
+            <div tabIndex={0} role="tabpanel" id="speedy-searching-tab" aria-labelledby="speedy-searching">
+
+            </div>
+            <div tabIndex={0} role="tabpanel" id="easy-sharing-tab" aria-labelledby="easy-sharing">
+
             </div>
           </div>
         </section>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import { ComponentPropsWithoutRef } from "react";
 const colors: Record<ThemeColor, string> = {
   'soft-red': 'bg-soft-red hover:text-soft-red hover:border-soft-red',
   'soft-blue': 'bg-soft-blue hover:text-soft-blue hover:border-soft-blue',
-  'gray': 'text-gray-600 bg-gray-100 hover:text-white hover:border-gray-600 hover:bg-gray-600'
+  'gray': '!text-gray-600 bg-gray-100 hover:text-white hover:border-gray-600 hover:bg-gray-600'
 }
 type ThemeColor = 'soft-red' | 'soft-blue' | 'gray';
 
@@ -12,7 +12,7 @@ const Button = ({ themeColor, className, ...otherProps }: ComponentPropsWithoutR
   const colorClasses = themeColor ? colors[themeColor] : colors['soft-blue'];
 
   return (
-    <button className={`${colorClasses} md:px-8 md:py-2 px-4 py-2 rounded-md text-[white] shadow-md hover:bg-white border-transparent border-solid border-2 ${className} bg-gra`} {...otherProps}>
+    <button className={`md:px-8 md:py-2 px-4 py-2 rounded-md text-white shadow-md hover:bg-white border-transparent border-solid border-2 ${colorClasses} ${className}`} {...otherProps}>
     </button>
   )
 }
